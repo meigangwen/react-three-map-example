55907b3e8ab13820e67c6697acd5507a9b6efa8a
fancilivain <meigangwen@gmail.com>
2024-02-19 17:42:18 +0800

    changed something

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useFrame, Vector3 } from "@react-three/fiber";
+import { Environment } from "@react-three/drei";
 import "maplibre-gl/dist/maplibre-gl.css";
 import { FC, useRef, useState } from "react";
 import Map from "react-map-gl/maplibre";
@@ -36,6 +37,38 @@ const Box: FC<{ position: Vector3 }> = (props) => {
 
 export default function Home() {
   return (
+    
+      <Map
+        antialias
+        initialViewState={{
+          latitude: 51,
+          longitude: 0,
+          zoom: 13,
+          pitch: 60,
+        }}
+        mapStyle="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
+      >
+        <Canvas latitude={51} longitude={0}>
+          <hemisphereLight
+            args={["#ffffff", "#60666C",3.14]}
+            position={[1, 4.5, 3]}
+          />
+          <object3D scale={500}>
+            <Box position={[-1.2, 1, 0]} />
+            <Box position={[1.2, 1, 0]} />
+          </object3D>
+          <group scale={20}>
+            <Environment preset="city" background />
+          </group>
+        </Canvas>
+      </Map>
+    
+  );
+}
+
+/*
+export default function Home() {
+  return (
     <div style={{ position: "absolute", left: 0, right: 0, top: 0, bottom: 0 }}>
       <Map
         antialias
@@ -49,15 +82,17 @@ export default function Home() {
       >
         <Canvas latitude={51} longitude={0}>
           <hemisphereLight
-            args={["#ffffff", "#60666C"]}
+            args={["#ffffff", "#60666C",3.14]}
             position={[1, 4.5, 3]}
           />
           <object3D scale={500}>
             <Box position={[-1.2, 1, 0]} />
             <Box position={[1.2, 1, 0]} />
           </object3D>
+          <Environment preset="city" background />
         </Canvas>
       </Map>
     </div>
   );
 }
+*/
